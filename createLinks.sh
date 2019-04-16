aed224abb28701b2e005b79ce94c4fc288155084
Jeff Milner <j.milner359@gmail.com>
2019-04-16 05:57:26 +0000

    Init commit

---
+++ createLinks.sh
@@ -0,0 +1,50 @@
+#!/bin/zsh
+#===============================================================================
+#
+#             NOTES: For this to work you must have cloned the github
+#                    repo to your home folder as ~/dotfiles/
+#
+#===============================================================================
+
+#==============
+# Variables
+#==============
+dotfiles_dir=~/dotfiles/confFiles
+
+#==============
+# Delete existing dot files and folders
+#==============
+sudo rm -rf ~/.aliases > /dev/null 2>&1
+sudo rm -rf ~/.bashrc > /dev/null 2>&1
+sudo rm -rf ~/.gitconfig > /dev/null 2>&1
+sudo rm -rf ~/.git-prompt.sh > /dev/null 2>&1
+sudo rm -rf ~/.inputrc > /dev/null 2>&1
+sudo rm -rf ~/.profile > /dev/null 2>&1
+sudo rm -rf ~/.tmux.conf > /dev/null 2>&1
+sudo rm -rf ~/.vim > /dev/null 2>&1
+sudo rm -rf ~/.vimrc > /dev/null 2>&1
+sudo rm -rf ~/.zshrc > /dev/null 2>&1
+
+#==============
+# Create symlinks in the home folder
+#==============
+ln -sf $dotfiles_dir/aliases        ~/.aliases
+ln -sf $dotfiles_dir/bashrc         ~/.bashrc
+ln -sf $dotfiles_dir/git/gitconfig      ~/.gitconfig
+ln -sf $dotfiles_dir/zsh/git-prompt.sh  ~/.git-prompt.sh
+ln -sf $dotfiles_dir/inputrc        ~/.inputrc
+ln -sf $dotfiles_dir/profile        ~/.profile
+ln -sf $dotfiles_dir/tmux.conf      ~/.tmux.conf
+ln -sf $dotfiles_dir/vim/.vim           ~/.vim
+ln -sf $dotfiles_dir/vim/vimrc          ~/.vimrc
+ln -sf $dotfiles_dir/zsh/zshrc          ~/.zshrc
+
+#==============
+# Set zsh as the default shell
+#==============
+#sudo chsh -s /bin/zsh
+
+#==============
+# Give the user a summary of what has been installed
+#==============
+echo -e "\nDone\n"
---
+++ install-packages.sh
@@ -0,0 +1,49 @@
+log_file=~/install_progress_log.txt
+
+sudo apt-get -y install zsh
+if type -p zsh > /dev/null; then
+    echo "zsh Installed" >> $log_file
+else
+    echo "zsh FAILED TO INSTALL!!!" >> $log_file
+fi
+
+sudo apt-get install zsh-syntax-highlighting
+
+sudo apt-get -y install curl
+if type -p curl > /dev/null; then
+    echo "curl Installed" >> $log_file
+else
+    echo "curl FAILED TO INSTALL!!!" >> $log_file
+fi
+
+
+# ---
+# Install git-completion and git-prompt
+# ---
+cd ~/
+curl -OL https://github.com/git/git/raw/master/contrib/completion/git-completion.bash
+mv ~/git-completion.bash ~/.git-completion.bash
+curl https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.git-prompt.sh
+echo "git-completion and git-prompt Installed and Configured" >> $log_file
+
+
+sudo apt-get -y install tmux
+if type -p tmux > /dev/null; then
+    echo "tmux Installed" >> $log_file
+else
+    echo "tmux FAILED TO INSTALL!!!" >> $log_file
+fi
+
+#==============
+# Set zsh as the default shell
+#==============
+sudo chsh -s /bin/zsh
+
+
+#==============
+# Give the user a summary of what has been installed
+#==============
+echo -e "\n====== Summary ======\n"
+cat $log_file
+echo
+rm $log_file
