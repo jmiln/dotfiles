1438247e58ef5c6fe1fec6a478162bf7d1c8ef32
Jeff Milner <j.milner359@gmail.com>
2020-08-18 14:34:37 -0700

    Some changes

--- install-packages.sh
+++ install-packages.sh
@@ -30,8 +30,8 @@ fi
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 echo "NVM downloaded"
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 nvm install node
 echo "Node/npm installed"
