857a9f6503a96cceb0ba65aba3b00e45c34a846c
jmiln <j.milner359@gmail.com>
2022-09-12 17:01:09 -0700

    Wipe out some legacy vim-plug stuff from the neovim dirs

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,15 +1,3 @@
--- " Enable filetype plugins and indention
--- filetype off
--- filetype plugin on
--- filetype plugin indent on       " Turn on filetype specific options
-
--- Install vim-plug if it's not already there
-local fn = vim.fn
-if fn.empty(fn.glob('~/.config/nvim/autoload/plug.vim')) > 0 then
-    fn.system({"silent", "!curl", "-fLo",  "~/.config/nvim/autoload/plug.vim", "--create-dirs", "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"})
-    vim.cmd [[PlugInstall]]
-end
-
 -- Load up whatever lua stuff
 require("settings")
 
