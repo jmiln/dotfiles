eff1a9798a3b843bb3eeccbb706f9731400f473e
jmiln <j.milner359@gmail.com>
2023-01-09 13:40:42 -0800

    Add another mongo funct, and change some more vim -> lua

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -122,7 +122,16 @@ opt.formatoptions = opt.formatoptions
   + "j" -- Auto-remove comments if possible.
 
 -- go to last loc when opening a buffer
-cmd([[au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | execute "normal! g`\"" | endif]])
+-- cmd([[au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | execute "normal! g`\"" | endif]])
+vim.api.nvim_create_autocmd('BufReadPost', {
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
 
 -- Set filetypes for various file extensions
 cmd([[au BufNewFile,BufRead *.php,*.html,*.css setlocal nocindent smartindent]])
