88c884a49e414807ab122e1fede4f8743873b788
jmiln <j.milner359@gmail.com>
2025-10-28 00:49:30 +0000

    feat: Got tmux/nvim integration somewhat working

--- nvim/lua/plugins/tmux-navigator.lua
+++ nvim/lua/plugins/tmux-navigator.lua
@@ -13,5 +13,21 @@ return {
     --         { "<M-Down>",  "<cmd>:TmuxNavigateDown<cr>" },
     --         { "<M-Up>",    "<cmd>:TmuxNavigateUp<cr>" },
     --     },
-    -- }
+    -- },
+    {
+        "alexghergh/nvim-tmux-navigation",
+        enabled = vim.fn.executable("tmux") == 1,
+        event = vim.env.TMUX ~= nil and "VeryLazy" or nil,
+        opts = {
+            disable_when_zoomed = true,
+            keybindings = {
+                left = "<M-Left>",
+                down = "<M-Down>",
+                up = "<M-Up>",
+                right = "<M-Right>",
+                -- last_active = "<M-\\>",
+                -- next = "<C-Space>",
+            },
+        },
+    },
 }
