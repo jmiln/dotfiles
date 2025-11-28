e0091f60f7d3b18834df5154756ac9a3bdb6f1aa
jmiln <j.milner359@gmail.com>
2025-11-28 15:39:11 -0800

    chore: Clean up hover.nvim config

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,43 +2,24 @@ return {
     {
         "lewis6991/hover.nvim",
         keys = {
-            {
-                mode = "n",
-                "K",
-                function()
-                    require("hover").hover()
-                end,
-                desc = "Show hover desc",
-            },
-            {
-                mode = "n",
-                "gK",
-                function()
-                    require("hover").hover_select()
-                end,
-                desc = "Show hover desc (select)",
-            },
+            { "K",     "<cmd>lua require('hover').hover()<cr>",                  desc = "Show hover desc" },
+            { "gK",    "<cmd>lua require('hover').hover_select()<cr>",           desc = "Show hover desc (select)" },
+            { "<C-p>", "<cmd>lua require('hover').hover_switch('previous')<cr>", desc = "hover.nvim (previous source)" },
+            { "<C-n>", "<cmd>lua require('hover').hover_switch('next')<cr>",     desc = "hover.nvim (next source)" },
         },
         opts = {
-            init = function()
-                require("hover.providers.diagnostic")
-                require("hover.providers.lsp")
-                require("hover.providers.gh")
-                require("hover.providers.gh_user")
-
-                vim.keymap.set("n", "K", require("hover").hover, { desc = "hover.nvim" })
-                vim.keymap.set("n", "gK", require("hover").hover_select, { desc = "hover.nvim (select)" })
-                vim.keymap.set("n", "<C-p>", function()
-                    require("hover").hover_switch("previous")
-                end, { desc = "hover.nvim (previous source)" })
-                vim.keymap.set("n", "<C-n>", function()
-                    require("hover").hover_switch("next")
-                end, { desc = "hover.nvim (next source)" })
-            end,
             preview_opts = {
                 border = "rounded",
             },
             preview_window = false,
+            providers = {
+                "hover.providers.diagnostic",
+                "hover.providers.lsp",
+                "hover.providers.man",
+                "hover.providers.dictionary",
+                "hover.providers.gh",
+                "hover.providers.gh_user",
+            },
             title = true,
         },
     },
@@ -49,15 +30,15 @@ return {
             "nvim-lua/plenary.nvim",
         },
         keys = {
-            { "<leader>fb", ":Telescope buffers<CR>", desc = "Search Buffers" },
-            { "<leader>fc", ":Telescope resume<CR>", desc = "Resume Last Search" },
-            { "<leader>ff", ":Telescope find_files<CR>", desc = "Search Files" },
-            { "<leader>fg", ":Telescope live_grep<CR>", desc = "Search File Text" },
-            { "<leader>fh", ":Telescope help_tags<CR>", desc = "Search Help" },
-            { "<leader>fr", ":Telescope registers<CR>", desc = "Search Registers" },
-            { "<leader>fm", ":Telescope marks<CR>", desc = "Search Marks" },
+            { "<leader>fb", ":Telescope buffers<CR>",        desc = "Search Buffers" },
+            { "<leader>fc", ":Telescope resume<CR>",         desc = "Resume Last Search" },
+            { "<leader>ff", ":Telescope find_files<CR>",     desc = "Search Files" },
+            { "<leader>fg", ":Telescope live_grep<CR>",      desc = "Search File Text" },
+            { "<leader>fh", ":Telescope help_tags<CR>",      desc = "Search Help" },
+            { "<leader>fr", ":Telescope registers<CR>",      desc = "Search Registers" },
+            { "<leader>fm", ":Telescope marks<CR>",          desc = "Search Marks" },
             { "<leader>fs", ":Telescope search_history<CR>", desc = "Search History" },
-            { "<leader>fz", ":Telescope spell_suggest<CR>", desc = "Spelling Suggest" },
+            { "<leader>fz", ":Telescope spell_suggest<CR>",  desc = "Spelling Suggest" },
         },
         opts = {
             defaults = {
