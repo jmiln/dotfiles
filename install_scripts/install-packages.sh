bc80267e00e17d2faa25aad76c733e05d287203e
jmiln <j.milner359@gmail.com>
2026-01-08 21:25:40 +0000

    chore: Move lazygit install to brew

--- install_scripts/install-packages.sh
+++ install_scripts/install-packages.sh
@@ -15,7 +15,7 @@ APT_PACKAGES=(
     build-essential curl git python3 software-properties-common
     unzip wget ripgrep fd-find jq jc bat
 )
-BREW_PACKAGES=(tmux eza git-delta)
+BREW_PACKAGES=(tmux eza git-delta lazygit)
 NPM_PACKAGES=(neovim npm-check-updates @biomejs/biome pm2)
 
 # -----
