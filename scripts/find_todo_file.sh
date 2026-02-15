7238ffbf75d60e5a31e4bed9ba99578203bfa45b
jmiln <j.milner359@gmail.com>
2026-02-15 20:37:18 +0000

    feat: Add local file checking for non-repo dirs

--- scripts/find_todo_file.sh
+++ scripts/find_todo_file.sh
@@ -12,25 +12,31 @@ PANE_PATH="$(tmux display-message -p -F "#{pane_current_path}")"
 TODO_FILE="$GLOBAL_TODO"
 LABEL=" Global Todo "
 
+# If in a Git repo, check for a root TODO
 if PROJECT_ROOT="$(git -C "$PANE_PATH" rev-parse --show-toplevel 2>/dev/null)"; then
-    # Grab just the directory name of the project
     PROJECT_NAME="${PROJECT_ROOT##*/}"
+    PROJECT_TODO=$(find "$PROJECT_ROOT" -maxdepth 1 -iname "todo.md" -o -iname "todo.txt" | head -n 1)
 
-    # Look for existing project todo
-    FIND_PROJECT_TODO=$(find "$PROJECT_ROOT" -maxdepth 1 -iname "todo.md" -o -iname "todo.txt" | head -n 1)
-
-    if [[ -n "$FIND_PROJECT_TODO" ]]; then
-        TODO_FILE="$FIND_PROJECT_TODO"
+    if [[ -n "$PROJECT_TODO" ]]; then
+        TODO_FILE="$PROJECT_TODO"
         LABEL=" Project: $PROJECT_NAME "
     else
         # If in a git repo but no project todo file exists yet, still use Global
-        TODO_FILE="$GLOBAL_TODO"
         LABEL=" Global (in $PROJECT_NAME) "
     fi
 fi
 
+# If it's still set to the global todo, check for a local todo
+if [[ "$TODO_FILE" == "$GLOBAL_TODO" ]]; then
+    LOCAL_SEARCH=$(find "$PANE_PATH" -maxdepth 1 -iname "todo.md" -o -iname "todo.txt" | head -n 1)
+
+    if [[ -n "$LOCAL_SEARCH" ]]; then
+        TODO_FILE="$LOCAL_SEARCH"
+        LABEL=" Local: ${PANE_PATH##*/} "
+    fi
+fi
+
 # Create pane and open editor
-# tmux display-popup -E -w 80% -h 80% -x C -y C "$EDITOR $TODO_FILE"
 tmux display-popup -E -w 95% -h 95% -d "$PANE_PATH" \
     -b rounded \
     -T "#[fg=orange,bold]$LABEL#[default]" \
